9409c5c0827d22cf1f7e92ce454a96957058ecf6
LeticiaBaiao <contatolbaiao@gmail.com>
2024-06-26 20:20:03 -0300

    FEAT: criação das rotas, funcionamento e telas

---
+++ sistema-teste/src/pages/Home/home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import * as C from "./home-styles";
+import Button from "../../components/Button/button";
+import useAuth from "../../hooks/useAuth";
+
+const Home = () => {
+  const { signout } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <C.Container>
+      <C.Title>Ínicio</C.Title>
+      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
+        Sair
+      </Button>
+    </C.Container>
+  );
+};
+
+export default Home;
---
+++ sistema-teste/src/pages/Signup/signup.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import * as C from "./signup-styles";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Button from "../../components/Button/button";
+import Input from "../../components/Input/input";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [emailConf, setEmailConf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const { signup } = useAuth();
+
+  const handleSignup = () => {
+    if (!email | !emailConf | !senha) {
+      setError("Preencha todos os campos");
+      return;
+    } else if (email !== emailConf) {
+      setError("Os e-mails não correspondem");
+      return;
+    }
+
+    const res = signup(email, senha);
+
+    if (res) {
+      setError(res);
+      return;
+    }
+
+    alert("Usuário cadatrado com sucesso!");
+    navigate("/");
+  };
+
+  return (
+    <C.Container>
+      <C.Label>SISTEMA DE LOGIN</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="email"
+          placeholder="Confirme seu email"
+          value={emailConf}
+          onChange={(e) => [setEmailConf(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua Senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.labelError>{error}</C.labelError>
+        <Button Text="Inscrever-se" onClick={handleSignup} />
+        <C.LabelSignin>
+          Já tem uma conta?
+          <C.Strong>
+            <Link to="/">&nbsp;Entre</Link>
+          </C.Strong>
+        </C.LabelSignin>
+      </C.Content>
+    </C.Container>
+  );
+};
+
+export default Signup;
